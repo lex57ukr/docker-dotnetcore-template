2f5a8abb3dde7361e31ab1fa3d8e5e2b49cbb48c
Alexander Akhtyrskiy <lex57ukr@gmail.com>
2018-09-10 10:48:14 -0600

    Add more configuration options

--- App.Tests/UnitTest1.cs
+++ App.Tests/UnitTest1.cs
@@ -8,7 +8,13 @@ namespace App.Tests
         [Fact]
         public void Test1()
         {
+            Console.WriteLine("Hello from xUnit (regular test)");
+        }
 
+        [Fact, Trait("category", "integration")]
+        public void Test2()
+        {
+            Console.WriteLine("Hello from xUnit (integration test)");
         }
     }
 }
